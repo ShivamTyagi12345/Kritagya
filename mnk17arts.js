893d5b228d4d5716e6234c612a257b57499a7f49
Madhapur Nitish Kumar <mnk17arts@gmail.com>
2021-08-25 22:09:43 +0530

    mnk17arts

---
+++ mnk17arts.js
@@ -0,0 +1,13 @@
+
+
+const projectName = "kritagya"
+
+var desc = "here goes the description"
+
+const contributor = "mnk17arts"
+
+console.log("Project : ",projectName)
+console.log("Description : ",desc)
+console.log("Contributed by : ",contributor)
+
+
